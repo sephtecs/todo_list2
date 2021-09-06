5e6ff4b29ed60164bac288c2bd66f87927710170
Joseph <josephtecson18@gmail.com>
2021-09-05 19:11:57 -0700

    add files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const[newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleNewTodoSubmit = (event) => {
+    event.preventDefault();
+
+    if (newTodo.length === 0) {
+      return;
+    }
+
+    // setTodos and pass in a brand new array contaiing all current todos plus 1 more
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+  }
+
+  const handleTodoDelete = (delIdx) => {
+    const filteredTodos = todos.filter((todo, i) => {
+      return i !== delIdx;
+    });
+    
+    setTodos(filteredTodos);
+
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: "center"}}>
+      <form onSubmit={(event) => {
+        handleNewTodoSubmit(event);
+      }}>
+        <input onChange = {(event) => {
+          setNewTodo(event.target.value);
+        }} type="text"
+        value={newTodo} />
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
+
+      <hr />
+
+      {todos.map((todo, i) => {
+        return <div>
+          <span>{todo}</span>
+          <button style={{marginLeft: "10px"}} onClick={(event) => {
+            handleTodoDelete(i);
+          }}>Delete</button>
+        </div>
+      })}
     </div>
   );
 }
